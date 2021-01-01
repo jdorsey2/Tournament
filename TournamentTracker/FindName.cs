9987dad18503296ec0b2e3a70783f3187c0eb6aa
Jason Dorsey <jason97dorsey@gmail.com>
2021-01-01 16:49:02 -0500

    fixed error checking

--- TournamentTracker/FindName.cs
+++ TournamentTracker/FindName.cs
@@ -6,72 +6,98 @@ namespace TournamentTracker
 {
     class FindName
     {
-        public static Team Search(string name, List<Team> teams) // fix error checking
+        public static Team Search(string name, List<Team> teams) 
         {
+            bool flag = false;
             bool keepGoing = true;
 
             while (keepGoing)
             {
                 keepGoing = false;
+
                 for (int i = 0; i < teams.Count; i++)
                 {
                     if (name == teams[i].name)
                     {
+                        flag = true;
                         return teams[i];
+
                     }
-                    
-                } 
+                }
+                if (flag == false)
+                {
+                    Console.WriteLine("try again");
+                    name = Console.ReadLine();
+                    name = name.ToUpper();
+                    name = ErrorChecking.EnsureEmptyLines(name);
+                    name = ErrorChecking.EnsureLength(name);
+                    keepGoing = true;
+                }
             }
             return new Team();
         }
 
         public static Match Search(string name, List<Match> matches)
         {
+            bool flag = false;
             bool keepGoing = true;
+
             while (keepGoing)
             {
                 keepGoing = false;
+
                 for (int i = 0; i < matches.Count; i++)
                 {
                     if (name == matches[i].name)
                     {
+                        flag = true;
                         return matches[i];
-                    }else
-                    {
-                        keepGoing = true;
-                        Console.WriteLine("Please enter a name which exists in the system, or add it");
-                        name = Console.ReadLine();
-                        name = name.ToUpper();
-                        name = ErrorChecking.EnsureEmptyLines(name);
-                        name = ErrorChecking.EnsureLength(name);
                     }
-                } 
+                }
+
+                if (flag == false)
+                {
+                    Console.WriteLine("try again");
+                    name = Console.ReadLine();
+                    name = name.ToUpper();
+                    name = ErrorChecking.EnsureEmptyLines(name);
+                    name = ErrorChecking.EnsureLength(name);
+                    keepGoing = true;
+                }
             }
+            
             return new Match();
         }
 
         public static Match SearchForTeamToEnterScore(string name, List<Match> matches)
         {
+            bool flag = false;
             bool keepGoing = true;
+
             while (keepGoing)
             {
                 keepGoing = false;
+
                 for (int i = 0; i < matches.Count; i++)
                 {
                     if (name == matches[i].firstTeam.name || name == matches[i].secondTeam.name)
                     {
+                        flag = true;
                         return matches[i];
-                    }else
-                    {
-                        keepGoing = true;
-                        Console.WriteLine("Please try again");
-                        name = Console.ReadLine();
-                        name = name.ToUpper();
-                        name = ErrorChecking.EnsureEmptyLines(name);
-                        name = ErrorChecking.EnsureLength(name);
                     }
-                } 
-            }
+                }
+
+                if (flag == false)
+                {
+                    Console.WriteLine("try again");
+                    name = Console.ReadLine();
+                    name = name.ToUpper();
+                    name = ErrorChecking.EnsureEmptyLines(name);
+                    name = ErrorChecking.EnsureLength(name);
+                    keepGoing = true;
+                }
+            } 
+            
             return new Match();
         }
     }
